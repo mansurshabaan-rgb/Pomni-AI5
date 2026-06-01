ab04d18992528a84aea1495c1e166c0f7ee313d3
mansurshabaan-rgb <mansurshabaan@gmail.com>
2026-06-01 13:22:15 +0300

    Update index.js

--- index.js
+++ index.js
@@ -5,14 +5,14 @@ import sub from './sub.js';
 
 /* =========== Client ========== */
 const client = new Client({
-  phoneNumber: '20123456789', // Bot number
+  phoneNumber: '201557346292', // Bot number
   prefix: [".", "/", "!"],
-  fromMe: false, 
+  fromMe: true, 
   owners: [
   // Owner 1
-    { name: "VA", lid: "247579682029763@lid", jid: "972569311531@s.whatsapp.net" },
+    { name: "𝕸𝖚𝖘𝖙𝖆𝖋𝖆", lid: "201557346292@lid", jid: "201557346292@s.whatsapp.net" },
   // Owner 2
-    { name: "emam", lid: "221307316789354@lid", jid: "201144480436@s.whatsapp.net" },
+    { name: "𝕸𝖚𝖘𝖙𝖆𝖋𝖆", lid: "201208197039@lid", jid: "201208197039@s.whatsapp.net" },
   // Owner 3
     { name: "Sukuna", jid: "201033024135@s.whatsapp.net", lid: "50414477168824@lid" },
   // Owner 4 
@@ -33,8 +33,8 @@ if (!global.db) {
 /* =========== Config ========== */
 const { config } = client;
 config.info = { 
-  nameBot: "♡ 𝙋𝙊𝙈𝙉𝙄 🎪 〈", 
-  nameChannel: "𝐕𝐈𝐈7 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕷️", 
+  nameBot: "♡ 𝕸𝖚𝖘𝖙𝖆𝖋𝖆 🕸 〈", 
+  nameChannel: "𝖁𝖆 ~ 𝐂𝐡𝐚𝐧𝐧𝐞𝐥 🕸", 
   idChannel: "120363225356834044@newsletter",
   urls: {
     repo: "https://github.com/deveni0/Pomni-AI",
@@ -42,8 +42,8 @@ config.info = {
     channel: "https://whatsapp.com/channel/0029VaQim2bAu3aPsRVaDq3v"
   },
   copyright: { 
-    pack: 'ڤـ ـ VA ـ ـا', 
-    author: 'VA'
+    pack: '𝕸𝖚𝖘𝖙𝖆𝖋𝖆', 
+    author: '𝕾𝖆'
   },
   images: [
     "https://i.pinimg.com/originals/11/26/97/11269786cdb625c60213212aa66273a9.png",
